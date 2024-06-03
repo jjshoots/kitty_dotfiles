5074320f0a026c05cc78e8f556d48da2315a7967
Jet <taijunjet@hotmail.com>
2024-06-04 00:01:22 +0900

    grammar

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-# install kitty
-sudo apt install kitty
+# try to install kitty if it doesn't already exist
+if ! command -v kitty &> /dev/null
+then
+  echo "kitty not found, installing from apt..."
+  sudo apt update
+  sudo apt install kitty
+fi
 
 # set this as the default terminal
 gsettings set org.gnome.desktop.default-applications.terminal exec 'kitty'
